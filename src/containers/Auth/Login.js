03396d81cb9078025aa5d22121c340f0a69ead01
NT Thanh Ngan <ntthanhngan19821@gmail.com>
2022-09-16 23:55:56 +0700

    connect server with login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -36,6 +36,7 @@ class Login extends Component {
 
         try {
             let data = await handleLoginApi(this.state.username, this.state.password)
+            console.log(data)
             if (data && data.errCode !== 0) {
                 this.setState({
                     errMessage: data.message
@@ -64,64 +65,68 @@ class Login extends Component {
 
     render() {
         return (
-            <form className='d-flex justify-content-center align-items-center flex-column'>
-                <h2>Login</h2>
-                <div className="form-outline mb-3 col-7 form-floating">
-                    <input type="text" id="username" className="form-control"
-                        value={this.state.username}
-                        onChange={(event) => { this.handleOnChangeUsername(event) }}
-                    />
-                    <label className="form-label" htmlFor="username">Username</label>
-                </div>
-
-                <div className="form-outline mb-3 col-7 form-floating">
-                    <input type={this.state.isShowPassword ? 'password' : 'text'}
-                        id="password" className="form-control"
-                        value={this.state.password}
-                        onChange={(event) => { this.handleOnChangePassword(event) }}
-                    />
-                    <label className="form-label" htmlFor="password">Password</label>
-                </div>
-                <div className='col-12'>
-                    <span className='error-message'>{this.state.errMessage}</span>
-                </div>
-                <div className="col-7">
-                    <div className="form-check">
-                        <input className="form-check-input"
-                            type='checkbox'
-                            onChange={() => this.handleShowHidePassword()}
-                            value="" id="form2Example31" />
-                        <label className="form-check-label" htmlFor="form2Example31"> Show/Hide password </label>
+            <div className='container'>
+                <form className='d-flex justify-content-center align-items-center flex-column'>
+                    <h2>Login</h2>
+                    <div className="form-outline mb-3 col-7 form-floating">
+                        <input type="text" id="username" className="form-control"
+                            value={this.state.username}
+                            onChange={(event) => { this.handleOnChangeUsername(event) }}
+                        />
+                        <label className="form-label" htmlFor="username">Username</label>
                     </div>
-                </div>
-                <div className='row mb-3 mt-3 col-7 d-flex justify-content-between align-items-center'>
-                    <div className="col-4 forgot-pwd">
-                        <a href="#!">Forgot password?</a>
+
+                    <div className="form-outline mb-2 col-7 form-floating">
+                        <input type={this.state.isShowPassword ? 'password' : 'text'}
+                            id="password" className="form-control"
+                            value={this.state.password}
+                            onChange={(event) => { this.handleOnChangePassword(event) }}
+                            autoComplete='true'
+                        />
+                        <label className="form-label" htmlFor="password">Password</label>
                     </div>
-                    <button type="button" className="btn btn-primary btn-block mb-4 w-35 col-4 btn-signin"
-                        onClick={() => this.handleLogin()}
-                    >
-                        Sign in
-                    </button>
-                </div>
+                    <div className='col-7 mb-3'>
+                        <span className='error-message'>{this.state.errMessage}</span>
+                    </div>
+                    <div className="col-7">
+                        <div className="form-check">
+                            <input className="form-check-input"
+                                type='checkbox'
+                                onChange={() => this.handleShowHidePassword()}
+                                value="" id="form2Example31" />
+                            <label className="form-check-label" htmlFor="form2Example31"> Show/Hide password </label>
+                        </div>
+                    </div>
+                    <div className='row mb-3 mt-3 col-7 d-flex justify-content-between align-items-center'>
+                        <div className="col-4 forgot-pwd">
+                            <a href="#!">Forgot password?</a>
+                        </div>
+                        <button type="button" className="btn btn-primary btn-block mb-4 w-35 col-4 btn-signin"
+                            onClick={() => this.handleLogin()}
+                        >
+                            Sign in
+                        </button>
+                    </div>
 
 
-                <div className="text-center">
-                    <h4>or sign in with:</h4>
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                        <i className="fab fa-facebook-f"></i>
-                    </button>
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                        <i className="fab fa-google"></i>
-                    </button>
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                        <i className="fab fa-twitter"></i>
-                    </button>
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                        <i className="fab fa-github"></i>
-                    </button>
-                </div>
-            </form>
+                    <div className="text-center">
+                        <h4>or sign in with:</h4>
+                        <button type="button" className="btn btn-link btn-floating mx-1">
+                            <i className="fab fa-facebook-f"></i>
+                        </button>
+                        <button type="button" className="btn btn-link btn-floating mx-1">
+                            <i className="fab fa-google"></i>
+                        </button>
+                        <button type="button" className="btn btn-link btn-floating mx-1">
+                            <i className="fab fa-twitter"></i>
+                        </button>
+                        <button type="button" className="btn btn-link btn-floating mx-1">
+                            <i className="fab fa-github"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+
 
         )
     }
